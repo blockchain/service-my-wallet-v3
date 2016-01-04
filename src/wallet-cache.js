b565f31cc2a1ddad282916976d2388c9b2baf223
Justin Tormey <jrtormey@gmail.com>
2016-01-04 16:34:02 -0500

    fix ugly spacing

--- src/wallet-cache.js
+++ src/wallet-cache.js
@@ -92,6 +92,6 @@ function safeReset() {
 }
 
 function generatePwHash(pw) {
-    var iterations = 5000;
+  var iterations = 5000;
   return crypto.pbkdf2Sync(pw, randomBytes, iterations, BYTES_PER_HASH, 'sha256');
 }
