166379bee41c7da9acc3c9ce1683afb0e3d155e2
Justin Tormey <jrtormey@gmail.com>
2016-02-24 12:12:54 -0500

    fix issue with retreiving balance of account

--- src/api.js
+++ src/api.js
@@ -231,7 +231,7 @@ function requireSecondPassword(options) {
 
 function formatAcct(a) {
   return !(a instanceof Object) ? undefined : {
-    label: a.label, index: a.index, archived: a.archived,
+    balance: a.balance, label: a.label, index: a.index, archived: a.archived,
     extendedPublicKey: a.extendedPublicKey, extendedPrivateKey: a.extendedPrivateKey,
     receiveIndex: a.receiveIndex, lastUsedReceiveIndex: a.lastUsedReceiveIndex,
     receivingAddressLabels: a.receivingAddressesLabels
