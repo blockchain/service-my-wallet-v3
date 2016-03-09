f5d86215667d4d2e2edafd9945249157b721db85
Justin Tormey <jrtormey@gmail.com>
2016-03-09 18:17:51 -0500

    handle top level exceptions gracefully

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ winston.level = process.env.LOGLEVEL || 'info';
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, { timestamp: Date.now });
 
+// handle top level exceptions
+process.on('uncaughtException', winston.error);
+
 var server  = require('./src/server')
   , rpc     = require('./src/rpc-server');
 
