675e1127edb950bfafae5810219fcf98d57ecc8c
Justin Tormey <justin@blockchain.com>
2017-09-18 14:06:52 -0400

    fix route matching for v2 api wallet creation

--- src/server.js
+++ src/server.js
@@ -174,7 +174,7 @@ v2API.use(parseOptions({
 }))
 
 v2API.all(
-  /\/create|\/create_wallet/,
+  ['/create', '/create_wallet'],
   required(['password', 'api_code']),
   function (req, res) {
     var apiAction = api.createWallet(req.options)
